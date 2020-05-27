33a0f373a01e10d7d2389e01c0f1c64aa1c9d1e2
mattcleary55 <mattcleary55@gmail.com>
2020-05-27 16:50:49 +0000

    add asset serving in node server

--- server/server.ts
+++ server/server.ts
@@ -1,10 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const getCheckInHistory = require('./endpoints/getCheckInHistory');
 const logCheckIn = require('./endpoints/logCheckIn');
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 // parse application/json
 app.use(bodyParser.json());
